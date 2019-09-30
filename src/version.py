93ba75b4a985ea5365667d5efe2721211327fb3e
Piradeep Kandasamy <piradeep@amazon.com>
2019-09-30 13:22:16 -0700

    Update to use the GO SDK v1.25.2
    cr https://code.amazon.com/reviews/CR-13831768

--- src/version.py
+++ src/version.py
@@ -3,4 +3,4 @@ See the most recent version number for AWS SDK Go here:
 https://github.com/aws/aws-sdk-go/releases
 """
 schema_version = 'v1.1.0'
-sdk_go_version = 'v1.24.6'
+sdk_go_version = 'v1.25.2'
